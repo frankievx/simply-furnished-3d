ecc66030975f3d9b6075430df161f3c4c69d7a45
Francis <frankievx@gmail.com>
2024-03-25 14:56:18 -0600

    working related products nav

--- src/app/[productId]/related/components/TheRelatedProductsNavigation.tsx
+++ src/app/[productId]/related/components/TheRelatedProductsNavigation.tsx
@@ -1,16 +1,10 @@
 "use client";
-import {
-  products,
-  productsApiAtom,
-  relatedProductsApiAtom,
-} from "@/state/products";
-import { useAtom, useAtomValue, useSetAtom } from "jotai";
+import { products } from "@/state/products";
 import { useParams, useRouter } from "next/navigation";
-import { MutableRefObject, useEffect, useRef } from "react";
-import { cameraSpringAtom } from "@/state/camera";
-import { showAtom } from "@/state/show";
+import { MutableRefObject, useEffect } from "react";
 import { useHorizontalDragGestures } from "../hooks/useHorizontalDragGestures";
-import { animated, useSprings, useTransition } from "@react-spring/web";
+import { animated, useSpring, useSprings } from "@react-spring/web";
+import { times } from "lodash";
 
 export const TheRelatedProductsNavigation = ({
   productId,
@@ -20,86 +14,7 @@ export const TheRelatedProductsNavigation = ({
   relatedProductIdRef: MutableRefObject<number>;
 }) => {
   const router = useRouter();
-  // const productId = Number(useParams().productId);
   const relatedProductId = Number(useParams().relatedProductId);
-  // const relatedProductIdRef = useRef(relatedProductId);
-
-  const navButtons = [
-    {
-      opacity: 0,
-      handler: () => {
-        const prevId =
-          relatedProductId === 0 ? products.length - 1 : relatedProductId - 1;
-        router.push(`/${productId}/related/${prevId}`);
-      },
-    },
-    {
-      opacity: 0.4,
-      className: "cursor-pointer",
-      handler: () => {
-        const prevId =
-          relatedProductId === 0 ? products.length - 1 : relatedProductId - 1;
-        router.push(`/${productId}/related/${prevId}`);
-      },
-    },
-    {
-      opacity: 1,
-      className: "cursor-default",
-    },
-    {
-      opacity: 0.4,
-      className: "cursor-pointer",
-      handler: () => {
-        const nextId =
-          relatedProductId === products.length - 1 ? 0 : relatedProductId + 1;
-        router.push(`/${productId}/related/${nextId}`);
-      },
-    },
-    {
-      opacity: 0,
-    },
-  ];
-
-  // const transitions = useTransition(navButtons, () => {
-  //   return
-  // });
-
-  // const [containerSpring, containerApi] = useSpring({})
-  const [springs, api] = useSprings(
-    5,
-    (i) => {
-      let opacity = 0.4;
-
-      if (i === 2) {
-        opacity = 1;
-      }
-
-      if (i === 1 || i === 3) opacity = 0.4;
-      relatedProductIdRef.current = relatedProductId;
-
-      return {
-        opacity,
-        x: "0px",
-      };
-    },
-    []
-  );
-  useEffect(() => {
-    const isPrev =
-      (relatedProductIdRef.current === 0 && relatedProductId === 7) ||
-      relatedProductId < relatedProductIdRef.current;
-    if (isPrev)
-      api.start((i) => {
-        if (i === 2) return { opacity: 0.4 };
-        if (i === 3) return { opacity: 1 };
-      });
-
-    // else
-    //   api.start((i) => ({
-    //     x: "100px",
-    //   }));
-    return () => console.log("nav unmounting");
-  }, [relatedProductId]);
 
   const nextHandler = () => {
     const nextId =
@@ -113,24 +28,128 @@ export const TheRelatedProductsNavigation = ({
     router.push(`/${productId}/related/${prevId}`);
   };
 
+  const navButtons = times(5, (i) => {
+    const navButton = {
+      opacity: 0.4,
+      className: "cursor-default",
+      handler: () => {},
+    };
+    if (i === 1) {
+      navButton.handler = prevHandler;
+      navButton.className = "cursor-pointer";
+    }
+    if (i === 2) navButton.opacity = 1;
+    if (i === 3) {
+      navButton.handler = nextHandler;
+      navButton.className = "cursor-pointer";
+    }
+    return navButton;
+  });
+
+  const [containerSpring, containerApi] = useSpring(
+    {
+      x: "0rem",
+      reset: true,
+    },
+    []
+  );
+
+  const [springs, api] = useSprings(
+    navButtons.length,
+    (i) => {
+      console.log("runnign");
+      return {
+        opacity: navButtons[i].opacity,
+      };
+    },
+    [relatedProductId]
+  );
+  useEffect(() => {
+    relatedProductIdRef.current = relatedProductId;
+  }, []);
+
+  const animateNext = () => {
+    api.start((i) => {
+      if (i === 1) return { opacity: 1 };
+      return { opacity: 0.4 };
+    });
+    containerApi.start({
+      x: "3.5rem",
+      config: { duration: 1500 },
+      onRest: () => {
+        containerApi.set({ x: "0rem" });
+        api.set((i) => {
+          if (i === 2) return { opacity: 1 };
+          return { opacity: 0.4 };
+        });
+      },
+    });
+    relatedProductIdRef.current = relatedProductId;
+  };
+
+  const animatePrev = () => {
+    api.start((i) => {
+      if (i === 1) return { opacity: 1 };
+      return { opacity: 0.4 };
+    });
+    containerApi.start({
+      x: "3.5rem",
+      config: { duration: 1500 },
+      onRest: () => {
+        containerApi.set({ x: "0rem" });
+        api.set((i) => {
+          if (i === 2) return { opacity: 1 };
+          return { opacity: 0.4 };
+        });
+      },
+    });
+    relatedProductIdRef.current = relatedProductId;
+  };
+
+  useEffect(() => {
+    if (
+      [relatedProductId, relatedProductIdRef.current].includes(0) &&
+      [relatedProductId, relatedProductIdRef.current].includes(7)
+    ) {
+      if (relatedProductIdRef.current === 7 && relatedProductId === 0)
+        animateNext();
+      if (relatedProductIdRef.current === 0 && relatedProductId === 7)
+        animatePrev();
+    } else {
+      if (relatedProductIdRef.current < relatedProductId) {
+        animateNext();
+      } else if (relatedProductIdRef.current > relatedProductId) {
+        animatePrev();
+      }
+    }
+  }, [relatedProductId]);
+
   useHorizontalDragGestures({ nextHandler, prevHandler });
 
   return (
     <>
       <div className="absolute text-white bottom-8 sm:bottom-20 w-full">
-        <div className="flex justify-center gap-4 w-40 overflow-hidden mx-auto">
-          {springs.map((props, i) => {
-            return (
-              <animated.button
-                key={"related-nav-button-" + i}
-                style={props}
-                className="cursor-pointer py-4"
-                onClick={prevHandler}
-              >
-                <div className="h-1 w-10 rounded-md bg-white"></div>
-              </animated.button>
-            );
-          })}
+        {/* Mask */}
+        <div className=" w-40 overflow-hidden mx-auto">
+          {/* Container */}
+          <animated.div
+            style={containerSpring}
+            className="flex justify-center w-full gap-4"
+          >
+            {springs.map((props, i) => {
+              return (
+                <animated.button
+                  id={"related-nav-button-" + i}
+                  key={"related-nav-button-" + i}
+                  style={props}
+                  className={"py-4 outline-none" + navButtons[i].className}
+                  onClick={navButtons[i].handler}
+                >
+                  <div className="h-1 w-10 rounded-md bg-white"></div>
+                </animated.button>
+              );
+            })}
+          </animated.div>
         </div>
       </div>
     </>
--- src/app/[productId]/related/layout.tsx
+++ src/app/[productId]/related/layout.tsx
@@ -1,16 +1,5 @@
 "use client";
-import {
-  products,
-  productsApiAtom,
-  relatedProductsApiAtom,
-} from "@/state/products";
-import { useAtom, useAtomValue, useSetAtom } from "jotai";
-import { useParams, usePathname, useRouter } from "next/navigation";
 import { ReactNode, useEffect, useRef } from "react";
-import { cameraSpringAtom } from "@/state/camera";
-import { showAtom } from "@/state/show";
-import { useHorizontalDragGestures } from "./hooks/useHorizontalDragGestures";
-import { animated, useSprings } from "@react-spring/web";
 import { TheRelatedProductsNavigation } from "./components/TheRelatedProductsNavigation";
 
 export default function RelatedProductsLayout({
@@ -21,27 +10,9 @@ export default function RelatedProductsLayout({
   params: { productId: string };
 }) {
   const relatedProductIdRef = useRef(0);
-
-  // const pathname = usePathname();
-  // console.log("pathname", pathname);
-  // const relatedProductId = Number(useParams().relatedProductId);
-  // const relatedProductIdRef = useRef(relatedProductId);
   const { productId } = {
     productId: Number(params.productId),
   };
-
-  // const nextHandler = () => {
-  //   const nextId =
-  //     relatedProductId === products.length - 1 ? 0 : relatedProductId + 1;
-
-  //   // router.push(`/${productId}/related/${nextId}`);
-  // };
-
-  // const prevHandler = () => {
-  //   const prevId =
-  //     relatedProductId === 0 ? products.length - 1 : relatedProductId - 1;
-  //   // router.push(`/${productId}/related/${prevId}`);
-  // };
 
   useEffect(() => {
     return () => console.log("layout unmount");
@@ -49,67 +20,11 @@ export default function RelatedProductsLayout({
 
   return (
     <>
-      {/* <div className="absolute text-white bottom-8 sm:bottom-20 w-full">
-        <div className=" flex justify-center gap-4"> */}
       <TheRelatedProductsNavigation
         productId={productId}
         relatedProductIdRef={relatedProductIdRef}
       />
-      {/* {springs.map((props, i) => {
-            if (i === 1)
-              return (
-                <animated.button
-                  style={props}
-                  className="cursor-pointer py-4"
-                  onClick={prevHandler}
-                >
-                  <div className="h-1 w-10 rounded-md bg-white"></div>
-                </animated.button>
-              );
-            if (i === 2)
-              return (
-                <animated.button
-                  style={props}
-                  className="cursor-default py-4 opacity-100"
-                >
-                  <div className="h-1 w-10 rounded-md bg-white"></div>
-                </animated.button>
-              );
-            if (i === 3)
-              return (
-                <animated.button
-                  style={props}
-                  className="cursor-default py-4 opacity-100"
-                  onClick={nextHandler}
-                >
-                  <div className="h-1 w-10 rounded-md bg-white"></div>
-                </animated.button>
-              );
-          })} */}
       {children}
-      {/* <button className="opacity-0 cursor-default" onClick={prevHandler}>
-            <div className="h-1 w-10 rounded-md bg-white"></div>
-          </button>
-          <button
-            className=" opacity-40 hover:opacity-100"
-            onClick={prevHandler}
-          >
-            <div className="h-1 w-10 rounded-md bg-white"></div>
-          </button>
-          <button className="cursor-default">
-            <div className="h-1 w-10 rounded-md bg-white"></div>
-          </button>
-          <button
-            className="opacity-40 hover:opacity-100 py-4"
-            onClick={nextHandler}
-          >
-            <div className="h-1 w-10 rounded-md bg-white"></div>
-          </button>
-          <button className="opacity-0 cursor-default" onClick={prevHandler}>
-            <div className="h-1 w-10 rounded-md bg-white"></div>
-          </button> */}
-      {/* </div>
-      </div> */}
     </>
   );
 }
